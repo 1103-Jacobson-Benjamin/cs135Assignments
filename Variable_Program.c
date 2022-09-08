f3105e6edc0a256e945f1712218a6665834466d0
1103-Jacobson-Benjamin <112990353+1103-Jacobson-Benjamin@users.noreply.github.com>
2022-09-07 21:49:07 -0700

    Add files via upload

---
+++ Variable_Program.c
@@ -0,0 +1,19 @@
+//Author: Benjamin Jacobson
+//Date: September 9th, 2022
+//Purpose: Program that holds and displays the letter grades expected on tests
+
+#include <stdio.h>
+int main(){
+	char testGradeOne = 'C';
+	char testGradeTwo = 'B';
+	char testGradeThree = 'A';
+	
+	printf("I am quite new to coding so hopefully this works\n");
+	
+	printf("Test one is probably going to be a %c\n", testGradeOne);
+	
+	printf("Hopefully test two is a %c\n", testGradeTwo);
+
+	printf("At this point I've probably studied a lot so test three should be an %c\n", testGradeThree);
+	return 0;
+}
